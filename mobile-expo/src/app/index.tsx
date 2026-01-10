8490b799cef964b874a0d2fc76cc55fd7d021971
EzioAuditore12 <dakshpurohit2005@gmail.com>
2026-01-11 03:54:55 +0530

    started trying sync with expo sqlite

---
+++ mobile-expo/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@/components/ui/text'
+import {View} from 'react-native'
+
+export default function Home(){
+    return(
+        <View className='flex-1 justify-center items-center'>
+            <Text variant={'h1'}>Hello</Text>
+        </View>
+    )
+}
